068d6ac4666704e3a1cb2cd0969f59aeab12afbb
pbk <impbk2002@gmail.com>
2025-04-05 00:39:12 +0900

    [iOS-custom] spine-c fix bound calculation with clipping and, fix rendering mesh uv count

--- spine-ios/Sources/spine-c/skeletonBounds.c
+++ spine-ios/Sources/spine-c/skeletonBounds.c
@@ -13,88 +13,119 @@ CF_INLINE void spSkeletonClipping_clipTriangles2( spSkeletonClipping *self, floa
 
 CF_RETURNS_RETAINED
 CGPathRef spSkeleton_createBoundingPath(spSkeleton *self, spSkeletonClipping * _Nullable clipper) {
-    unsigned short quadIndices[] = {0, 1, 2, 2, 3, 0};
+    spFloatArray *_worldVertices = spFloatArray_create(32);
+    const unsigned short quadIndices[] = {0, 1, 2, 2, 3, 0};
+    float region_vertices[] = {0,0,0,0,0,0,0,0};
+    spFloatArray uvsHolder = {0,0,0};
+    spUnsignedShortArray indicesHolder = {0,0,0};
 
 
-    spFloatArray* vertexBuffer = spFloatArray_create(8);
-    spFloatArray* uvBuffer = spFloatArray_create(32);
     CGMutablePathRef path = CGPathCreateMutable();
-	for (size_t i = 0; i < self->slotsCount; ++i) {
+    for (size_t i = 0; i < self->slotsCount; ++i) {
         spSlot *slot = self->drawOrder[i];
         if (!slot->bone->active)
-			continue;
-		int verticesLength = 0;
+            continue;
         spAttachment *attachment = slot->attachment;
-		unsigned short *triangles = NULL;
-        int trianglesLength = 0;
+        if (!attachment) {
+            if (clipper) {
+                spSkeletonClipping_clipEnd(clipper, slot);
+            }
+            continue;
+        }
+        if ((slot->color.a == 0 || !slot->bone->active) && attachment->type != SP_ATTACHMENT_CLIPPING) {
+            if (clipper) {
+                spSkeletonClipping_clipEnd(clipper, slot);
+            }
+            continue;
+        }
+        
+        spFloatArray *vertices = _worldVertices;
+        spFloatArray *uvs = &uvsHolder;
+        spUnsignedShortArray *indices = &indicesHolder;
+        int32_t verticesCount = 0;
 
-		if (attachment != NULL &&
-            attachment->type == SP_ATTACHMENT_REGION) {
+        if (attachment->type == SP_ATTACHMENT_REGION) {
             spRegionAttachment *regionAttachment = SUB_CAST(spRegionAttachment, attachment);
-			verticesLength = 8;
-            
-            if (vertexBuffer->size < verticesLength) {
-                spFloatArray_setSize(vertexBuffer, verticesLength);
-			}
-            
-            spRegionAttachment_computeWorldVertices(regionAttachment, slot, vertexBuffer->items, 0, 2);
-			triangles = quadIndices;
-			trianglesLength = 6;
-		} else if (attachment != NULL &&
-                   attachment->type == SP_ATTACHMENT_MESH) {
+            if (regionAttachment->color.a == 0) {
+                if (clipper) {
+                    spSkeletonClipping_clipEnd(clipper, slot);
+                }
+                continue;
+            }
+            spFloatArray_setSize(vertices, 8);
+            spRegionAttachment_computeWorldVertices(regionAttachment, slot, vertices->items, 0, 2);
+            *uvs = (spFloatArray){
+                .items = regionAttachment->uvs,
+                .size = 8,
+                .capacity = 8
+            };
+            verticesCount = 4;
+
+            *indices = (spUnsignedShortArray){
+                .items = (unsigned short*) quadIndices,
+                .size = 6,
+                .capacity = 6
+            };
+        } else if (attachment->type == SP_ATTACHMENT_MESH) {
             spMeshAttachment *mesh = SUB_CAST(spMeshAttachment, attachment);
 
-            
-            verticesLength = mesh->super.worldVerticesLength;
-            if (vertexBuffer->size < verticesLength) {
-                spFloatArray_setSize(vertexBuffer, verticesLength);
-			}
-
-            spVertexAttachment_computeWorldVertices(&mesh->super, slot, 0, verticesLength, vertexBuffer->items, 0, 2);
-			triangles = mesh->triangles;
-            
-			trianglesLength = mesh->trianglesCount;
-		} else if (attachment != NULL &&
-                   attachment->type == SP_ATTACHMENT_CLIPPING && clipper != NULL) {
-            spSkeletonClipping_clipStart(clipper, slot, SUB_CAST(spClippingAttachment, attachment));
-		}
-
-		if (verticesLength > 0) {
-            
-            float *vertices = vertexBuffer->items;
-			if (clipper != NULL && spSkeletonClipping_isClipping(clipper)) {
-                
-                spSkeletonClipping_clipTriangles2(clipper, vertices, vertexBuffer->size, triangles, trianglesLength, uvBuffer);
-                
-				vertices = clipper->clippedVertices->items;
-                
-				verticesLength = clipper->clippedVertices->size;
-			}
-            // add triagles to CGPath
-            for (int t = 0; t < trianglesLength; t += 3) {
-                int i0 = triangles[t] * 2;
-                int i1 = triangles[t + 1] * 2;
-                int i2 = triangles[t + 2] * 2;
-
-                CGPoint p0 = CGPointMake(vertices[i0], vertices[i0 + 1]);
-                CGPoint p1 = CGPointMake(vertices[i1], vertices[i1 + 1]);
-                CGPoint p2 = CGPointMake(vertices[i2], vertices[i2 + 1]);
-
-                CGPathMoveToPoint(path, NULL, p0.x, p0.y);
-                CGPathAddLineToPoint(path, NULL, p1.x, p1.y);
-                CGPathAddLineToPoint(path, NULL, p2.x, p2.y);
-                CGPathCloseSubpath(path);
+            if (mesh->color.a == 0) {
+                if (clipper) {
+                    spSkeletonClipping_clipEnd(clipper, slot);
+                }
+                continue;
             }
-		}
-        if (clipper != NULL) {
+            spFloatArray_setSize(vertices,mesh->super.worldVerticesLength);
+            spVertexAttachment_computeWorldVertices(&mesh->super, slot, 0, mesh->super.worldVerticesLength, vertices->items, 0, 2);
+            *uvs = (spFloatArray){
+                .items = mesh->uvs,
+                .size = mesh->super.worldVerticesLength,
+                .capacity = mesh->super.worldVerticesLength,
+            };
+            verticesCount = (int32_t) (mesh->super.worldVerticesLength >> 1);
+            *indices = (spUnsignedShortArray){
+                .items = mesh->triangles,
+                .size = mesh->trianglesCount,
+                .capacity = mesh->trianglesCount
+            };
+        } else if (attachment->type == SP_ATTACHMENT_CLIPPING && clipper != NULL) {
+            spClippingAttachment *clip = SUB_CAST(spClippingAttachment, attachment);
+            spSkeletonClipping_clipStart(clipper, slot, clip);
+            continue;
+        } else {
+            continue;
+        }
+
+        if (clipper != NULL && spSkeletonClipping_isClipping(clipper)) {
+            spSkeletonClipping_clipTriangles(clipper, vertices->items, verticesCount, indices->items, indices->size, uvs->items, 2);
+            vertices = clipper->clippedVertices;
+            uvs = clipper->clippedUVs;
+            indices = clipper->clippedTriangles;
+            verticesCount = clipper->clippedVertices->size >> 1;
+        }
+        // add triagles to CGPath
+        for (int t = 0; t < indices->size; t += 3) {
+            int i0 = indices->items[t] * 2;
+            int i1 = indices->items[t + 1] * 2;
+            int i2 = indices->items[t + 2] * 2;
+
+            CGPoint p0 = CGPointMake(vertices->items[i0], vertices->items[i0 + 1]);
+            CGPoint p1 = CGPointMake(vertices->items[i1], vertices->items[i1 + 1]);
+            CGPoint p2 = CGPointMake(vertices->items[i2], vertices->items[i2 + 1]);
+
+            CGPathMoveToPoint(path, NULL, p0.x, p0.y);
+            CGPathAddLineToPoint(path, NULL, p1.x, p1.y);
+            CGPathAddLineToPoint(path, NULL, p2.x, p2.y);
+            CGPathCloseSubpath(path);
+        }
+        if (clipper) {
             spSkeletonClipping_clipEnd(clipper, slot);
         }
     }
-    if (clipper != NULL) {
+    if (clipper) {
         spSkeletonClipping_clipEnd2(clipper);
     }
-    spFloatArray_dispose(vertexBuffer);
-    spFloatArray_dispose(uvBuffer);
+    spFloatArray_dispose(_worldVertices);
     CGPathRef final = CGPathCreateCopy(path);
     CFRelease(path);
     path = NULL;
