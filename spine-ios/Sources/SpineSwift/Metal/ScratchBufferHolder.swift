79f9ffe3356f134feb0d98c06f01de9c88170b50
pbk <impbk2002@gmail.com>
2025-05-09 19:37:31 +0900

    [iOS-custom] revert using MTLBufferProxy

---
+++ spine-ios/Sources/SpineSwift/Metal/ScratchBufferHolder.swift
@@ -0,0 +1,29 @@
+//
+//  ScratchBufferHolder.swift
+//  spine-ios
+//
+//  Created by 박병관 on 5/7/25.
+//
+
+#if canImport(Metal)
+import Metal
+
+final class ScratchBufferHolder:SpineVertexBuffer {
+    
+    let buffer: any MTLBuffer
+    let semaphore: DispatchSemaphore
+    
+    init(buffer: any MTLBuffer, semaphore: DispatchSemaphore) {
+        self.buffer = buffer
+        self.semaphore = semaphore
+    }
+    
+    deinit {
+        buffer.setPurgeableState(.empty)
+        semaphore.signal()
+    }
+    
+    let offsetInBytes: Int = 0
+    
+}
+#endif
