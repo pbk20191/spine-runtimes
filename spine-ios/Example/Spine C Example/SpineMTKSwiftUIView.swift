6d31b638301d2173ade97ab395295e11f7099aeb
pbk <impbk2002@gmail.com>
2025-07-05 08:28:35 +0900

    [iOS-custom] added binary format example case

--- spine-ios/Example/Spine C Example/SpineMTKSwiftUIView.swift
+++ spine-ios/Example/Spine C Example/SpineMTKSwiftUIView.swift
@@ -106,3 +106,88 @@ extension SpineMTKSwiftUIView: UIViewRepresentable {
     
 }
 #endif
+struct SpineMTKSwiftUIView2: Equatable {
+    
+    let atlasURL: URL
+    let skelURL: URL
+    let animationName: String?
+    let tag:String
+    
+    func makeCoordinator() -> SpineMTKViewDefaultDelegate {
+        let atlas = try! SpineAtlasBox(path: atlasURL.path)
+        let shared = try! SpineSkeletonDataBox(atlas: atlas, skelPath: skelURL.path)
+        let drawable = TaggedDrawable(resource: .init(skeletonData: shared))
+        drawable.setTag(tag)
+        if let animationName = self.animationName {
+            spAnimationState_setAnimationByName(&drawable.animationState, 0, animationName, 1)
+        }
+        let device = MTLCreateSystemDefaultDevice()!
+        let commandQueue = device.makeCommandQueue()!
+        let delegate = try! SpineMTKViewDefaultDelegate(drawable: drawable, commandQueue: commandQueue, pixelFormat: .bgra8Unorm)
+        return delegate
+    }
+    
+    func setupView(_ view :MTKView, context:Context) {
+        view.delegate = context.coordinator
+        view.device = context.coordinator.device
+        view.colorPixelFormat = .bgra8Unorm
+        view.clearColor = MTLClearColorMake(0, 0, 0, 0)
+        #if canImport(UIKit)
+        view.isOpaque = false
+        #else
+        view.layer?.isOpaque = false
+        #endif
+    }
+    
+    func updateView(_ view: MTKView, context: Context) {
+        let delegate = context.coordinator
+        let oldDrawable = delegate.drawable as! TaggedDrawable
+        if oldDrawable.tag != tag {
+            let atlas = try! SpineAtlasBox(path: atlasURL.path)
+            
+            let shared = try! SpineSkeletonDataBox(atlas: atlas, skelPath: skelURL.path)
+            let drawable = TaggedDrawable(resource: .init(skeletonData: shared))
+            drawable.setTag(tag)
+            delegate.drawable = drawable
+            if let animationName = self.animationName {
+                spAnimationState_setAnimationByName(&drawable.animationState, 0, animationName, 1)
+            }
+        }
+
+    }
+    
+}
+#if os(macOS)
+extension SpineMTKSwiftUIView2: NSViewRepresentable {
+    
+    func makeNSView(context: Context) -> MTKView {
+        let someView = MTKView()
+        setupView(someView, context: context)
+        return someView
+    }
+    
+    func updateNSView(_ nsView: MTKView, context: Context) {
+        updateView(nsView, context: context)
+
+    }
+    
+}
+#endif
+
+#if os(iOS) || os(visionOS) || os(tvOS)
+extension SpineMTKSwiftUIView2: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MTKView {
+        let someView = MTKView()
+
+        setupView(someView, context: context)
+        return someView
+    }
+    
+    func updateUIView(_ nsView: MTKView, context: Context) {
+        updateView(nsView, context: context)
+        
+    }
+    
+}
+#endif
--- spine-ios/Example/Spine C Example/Spine_C_ExampleApp.swift
+++ spine-ios/Example/Spine C Example/Spine_C_ExampleApp.swift
@@ -15,6 +15,11 @@ struct Spine_C_ExampleApp: App {
             NavigationView {
                 List {
                     NavigationLink {
+                        SpineMTKSwiftUIView2.init(atlasURL: Bundle.main.url(forResource: "tank-pma", withExtension: "atlas")!, skelURL: Bundle.main.url(forResource: "tank-pro", withExtension: "skel")!, animationName: "shoot", tag: "tank")
+                    } label: {
+                        Text("tank-pma")
+                    }
+                    NavigationLink {
                         SpineMTKSwiftUIView.init(atlasURL: Bundle.main.url(forResource: "coin-pma", withExtension: "atlas")!, jsonURL: Bundle.main.url(forResource: "coin-pro", withExtension: "json")!, animationName: "animation", tag: "coin")
                     } label: {
                         Text("coin-pma")
