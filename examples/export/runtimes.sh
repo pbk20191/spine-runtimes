814095fe11a33e2124926f2401818e260b075b83
Mario Zechner <badlogicgames@gmail.com>
2025-11-10 17:26:42 +0100

    Update runtime.sh for spine-godot extension

--- examples/export/runtimes.sh
+++ examples/export/runtimes.sh
@@ -241,6 +241,43 @@ cp -f ../celestial-circus/export/celestial-circus-pro.skel "$ROOT/spine-godot/ex
 cp -f ../celestial-circus/export/celestial-circus.atlas "$ROOT/spine-godot/example-v4-csharp/assets/celestial-circus/"
 cp -f ../celestial-circus/export/celestial-circus.png "$ROOT/spine-godot/example-v4-csharp/assets/celestial-circus/"
 
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/spineboy/*.atlas
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/spineboy/*.png
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/spineboy/*.spine-json
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/spineboy/*.skel
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/raptor/*.atlas
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/raptor/*.png
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/raptor/*.skel
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/mix-and-match/*.atlas
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/mix-and-match/*.png
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/mix-and-match/*.spine-json
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/mix-and-match/*.skel
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/raggedyspineboy/*.atlas
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/raggedyspineboy/*.png
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/raggedyspineboy/*.spine-json
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/celestial-circus/*.atlas
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/celestial-circus/*.png
+rm -f "$ROOT"/spine-godot/example-v4-extension/assets/celestial-circus/*.skel
+
+cp -f ../spineboy/export/spineboy-pro.json "$ROOT/spine-godot/example-v4-extension/assets/spineboy/spineboy-pro.spine-json"
+cp -f ../spineboy/export/spineboy-pro.skel "$ROOT/spine-godot/example-v4-extension/assets/spineboy/"
+cp -f ../spineboy/export/spineboy.atlas "$ROOT/spine-godot/example-v4-extension/assets/spineboy/"
+cp -f ../spineboy/export/spineboy.png "$ROOT/spine-godot/example-v4-extension/assets/spineboy/"
+
+cp -f ../mix-and-match/export/mix-and-match-pro.json "$ROOT/spine-godot/example-v4-extension/assets/mix-and-match/mix-and-match-pro.spine-json"
+cp -f ../mix-and-match/export/mix-and-match.atlas "$ROOT/spine-godot/example-v4-extension/assets/mix-and-match/"
+cp -f ../mix-and-match/export/mix-and-match.png "$ROOT/spine-godot/example-v4-extension/assets/mix-and-match/"
+
+cp -f ../raptor/export/raptor-pro.skel "$ROOT/spine-godot/example-v4-extension/assets/raptor/"
+cp -f ../raptor/export/raptor.atlas "$ROOT/spine-godot/example-v4-extension/assets/raptor/"
+cp -f ../raptor/export/raptor.png "$ROOT/spine-godot/example-v4-extension/assets/raptor/"
+cp -f ../raptor/manual-maps/raptor-normals.png "$ROOT/spine-godot/example-v4-extension/assets/raptor/n_raptor.png"
+cp -f ../raptor/manual-maps/light-sprite.png "$ROOT/spine-godot/example-v4-extension/assets/raptor/light-sprite.png"
+
+cp -f ../celestial-circus/export/celestial-circus-pro.skel "$ROOT/spine-godot/example-v4-extension/assets/celestial-circus/celestial-circus.skel"
+cp -f ../celestial-circus/export/celestial-circus.atlas "$ROOT/spine-godot/example-v4-extension/assets/celestial-circus/"
+cp -f ../celestial-circus/export/celestial-circus.png "$ROOT/spine-godot/example-v4-extension/assets/celestial-circus/"
+
 echo "spine-sdl"
 rm -f "$ROOT/spine-sdl/data/"*
 cp -f ../spineboy/export/spineboy-pro.json "$ROOT/spine-sdl/data/"
